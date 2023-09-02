ff1565265e857e450494828d0021c23e98f72eb9
DIVYA-BALU <divyabalusamy559@gmail.com>
2023-09-02 15:49:07 +0530

    fibo with bigint

--- fibo.java
+++ fibo.java
@@ -1,19 +1,25 @@
-import java.util.Scanner;
+import java.util.*;
+import java.math.*;
 class fibo{  
- static int n1=-1,n2=1,n3=0;    
- static void printFibonacci(int count){    
-    if(count>0){    
-         n3 = n1 + n2;    
-         n1 = n2;    
-         n2 = n3;    
-         System.out.print(" "+n3);   
-         printFibonacci(count-1);    
-     }    
+    static BigInteger n1 = BigInteger.valueOf(-1);
+    static BigInteger n2 = BigInteger.valueOf(1);
+    static BigInteger sum = BigInteger.valueOf(0);    
+    static void fibonacci(long count){   
+        if(count>=0){    
+            sum = n1.add(n2);    
+            n1 = n2;    
+            n2 = sum;    
+            System.out.print(" "+sum);   
+            fibonacci(count-1);    
+        }    
  }    
  public static void main(String args[]){    
   Scanner sc=new Scanner(System.in);
-  System.out.println("Enter range");
-  int count= sc.nextInt();   
-  printFibonacci(count);   
+  System.out.print("Enter no of terms : ");
+  long count= sc.nextLong();  
+  if(count<0)
+    System.out.println("Enter number greater than zero");
+  else
+    fibonacci(count);   
  }  
 }  
