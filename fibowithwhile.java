90473f00b83c9bfb92b9876d23a473c0707d774e
DIVYA-BALU <divyabalusamy559@gmail.com>
2023-09-03 09:09:41 +0530

    fibo

--- fibowithwhile.java
+++ fibowithwhile.java
@@ -1,33 +1,47 @@
 import java.util.*;
-import java.io.*;
 import java.math.*;
-class fibo{     
-    static void fibonacci(long n1,long n2,long count){   
-        if(count>=0){  
-            long sum = 0;  
+class fibowithwhile{   
+    static BigInteger f1 = BigInteger.valueOf(-1);
+    static BigInteger f2 = BigInteger.valueOf(1);
+    static boolean isInt(String s){
+      try
+        {
+            int num=Integer.parseInt(s);
+            return true;
+        }
+        catch (NumberFormatException e)
+        {
+            return false;
+        }
+    }  
+    static void fibonacci(BigInteger n1,BigInteger n2,long count){   
+        if(count>0){  
+            BigInteger sum = new BigInteger("0");  
             sum = n1.add(n2);    
             n1 = n2;    
             n2 = sum;  
-            System.out.print(" "+sum);//to print from begining to end     
+            System.out.print(" "+sum);    
             fibonacci(n1,n2,count-1);
-            //System.out.print(" "+sum);// to print from end to begining
-
+        }
+        else{
+          f1=n1;
+          f2=n2;
         }    
  }    
- public static void main(String args[]){   
-  Scanner sc=new Scanner(System.in);
-  long n1 = -1;
-  long n2 = 1; 
-  while(true){
-  System.out.print("Enter no of terms : ");
-  long count= sc.nextLong();  
-  if(count<0)
-    System.out.println("Enter number greater than zero");
-  else if(!Character.isDigit(count))
-    break;
-  else{
-     fibonacci(n1,n2,count); 
-  }
+    public static void main(String args[]){  
+      Scanner sc=new Scanner(System.in);
+      while(true){
+        System.out.print("\nEnter no of terms : ");
+        String count= sc.nextLine();  
+      if(isInt(count)){
+          int num= Integer.parseInt(count);
+          fibonacci(f1,f2,num);
+      }
+      else{
+        System.out.println("Exited");
+        break;
+      }
+   
   }
        
  }  
