7a0922e475bf4a0c00338bff3296309950430b77
DIVYA-BALU <divyabalusamy559@gmail.com>
2023-09-22 14:18:49 +0530

    merge sort

--- sort/mergesort.java
+++ sort/mergesort.java
@@ -3,8 +3,40 @@ package sort;
 import java.util.Scanner;
 
 public class mergesort {
+    static void merge(int arr[],int start,int middle,int end){
+        if(start==(end-1)){
+            if(arr[start]>arr[end]){
+                int temp=arr[start];
+                arr[start]=arr[end];
+                arr[end]=temp;
+            }
+            return;
+        }
+        else{
+            int nextStart=middle+1;
+            while(start<nextStart && nextStart<=end){
+                if(arr[start]>arr[nextStart]){
+                    int temp=arr[nextStart];
+                    for(int i=nextStart;i>start;i--){
+                        arr[i]=arr[i-1];
+                    }
+                    arr[start]=temp;
+                    start++;
+                    nextStart++;
+                }
+                else{
+                    start++;
+                }
+            }
+        }
+    }
     static void MergeSort(int arr[], int low,int high){
-        
+        if(low<high){
+            int mid=(low+high)/2;
+            MergeSort(arr, low, mid);
+            MergeSort(arr, mid+1, high);
+            merge(arr,low,mid,high);
+        }
     }
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
