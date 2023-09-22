f40189c70a6ae707fe4f42ba023e91121a9a80fd
DIVYA-BALU <divyabalusamy559@gmail.com>
2023-09-22 21:08:10 +0530

    collatz

---
+++ collatz.java
@@ -0,0 +1,25 @@
+
+
+public class collatz {
+    public static void main(String[] args) {
+        long max_len=0,start_num=0;
+        for(long i=1;i<1000000;i+=2){
+            long num=i;
+            long length=1;
+            while(num!=1){
+                if(num%2==0){
+                    num=num/2;
+                }
+                else{
+                    num=(3*num)+1;
+                }
+                length++; 
+            }
+            if(length>max_len){
+                start_num=i;
+                max_len=length;
+            }
+        }
+        System.out.println("The longest chain starts with "+start_num+" and the length of chain is "+max_len);
+    }
+}
