87049f5e80ebfad2a72043a6318d1f1c3399994b
DIVYA-BALU <divyabalusamy559@gmail.com>
2023-09-28 21:40:24 +0530

    updated store

--- database/online_store.sql
+++ database/online_store.sql
@@ -2,94 +2,70 @@ create database onlinestore;
 use onlinestore;
 
 create table author(
-URL varchar(50) unique,
 author_name varchar(20),
 address varchar(50) ,
+URL varchar(50),
 primary key(author_name,address)
 );
 
 create table publisher(
 publisher_name varchar(20) primary key,
 address varchar(50),
-phone integer,
+phone varchar(50),
 URL varchar(50)
 );
 
-create table item(
-item_id int primary key,
-item_name varchar(500),
-item_type varchar(50)
-);
 
-create table book(
-isbn varchar(10) primary key,
-item_id int,
-title varchar(20),
-book_year int,
+create table product(
+product_id varchar(10) primary key,
+product_name varchar(20),
+product_year int,
 price float4,
+product_type varchar(50),
 author_name varchar(20),
 address varchar(50) ,
 publisher_name varchar(20),
 foreign key(publisher_name) references publisher(publisher_name) ,
-foreign key(author_name,address) references author(author_name,address),
-foreign key(item_id) references item(item_id)
+foreign key(author_name,address) references author(author_name,address)
 );
-
-create table music_cassettes(
-cassettes_id varchar(10) primary key,
-item_id int,
-title varchar(20),
-cassettes_year int,
-price float4,
-author_name varchar(20),
-address varchar(50) ,
-publisher_name varchar(20),
-foreign key(publisher_name) references publisher(publisher_name) ,
-foreign key(author_name,address) references author(author_name,address),
-foreign key(item_id) references item(item_id)
-);
-
-create table compact_disk(
-disk_id varchar(10) primary key,
-item_id int,
-title varchar(20),
-disk_year int,
-price float4,
-author_name varchar(20),
-address varchar(50) ,
-publisher_name varchar(20),
-foreign key(publisher_name) references publisher(publisher_name) ,
-foreign key(author_name,address) references author(author_name,address),
-foreign key(item_id) references item(item_id)
-);
-
 
 
 create table warehouse(
-code varchar(10) primary key,
+w_code varchar(10) primary key,
 address varchar(50),
-phone integer,
-isbn varchar(10),
-foreign key(isbn) references book(isbn)
+phone varchar(50)
 );
+
+create table stocks(
+product_count int,
+w_code varchar(10),
+product_id varchar(10),
+foreign key(w_code) references warehouse(w_code),
+foreign key(product_id) references product(product_id)
+);
+
 
 create table customer(
 email varchar(50) primary key,
 name varchar(20),
 address varchar(50),
-phone integer
+phone varchar(50)
 );
-
 
 create table shopping_basket(
 basket_id varchar(10) primary key,
+total_price float4,
 email varchar(50),
 foreign key(email) references customer(email)
 );
 
-create table cart(
+create table cust_order(
+p_id varchar(10),
 basket_id varchar(10),
-item_id int,
-foreign key(item_id) references item(item_id),
+p_count int,
+price float4,
+w_code varchar(50),
+foreign key(w_code) references stocks(w_code),
 foreign key(basket_id) references shopping_basket(basket_id)
 );
+
